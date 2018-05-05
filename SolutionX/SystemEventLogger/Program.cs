3dc7242e0cd993d737ea1fe5922fb2df5450ed5d
Sean Lemster <sean.lemster@noa.nintendo.com>
2018-05-05 10:28:57 -0700

    Working sample code that writes a message to the System Event Log

--- SolutionX/SystemEventLogger/Program.cs
+++ SolutionX/SystemEventLogger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,7 +15,40 @@ namespace SystemEventLogger
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.ReadLine();
+            WriteToSystemEventLog();
         }
+
+        private static void WriteToSystemEventLog()
+        {
+            Console.WriteLine("Writing Test Message to System Event Log");
+            
+            try
+            {
+                var sSource = "ProjectX-SystemEventLogger";
+                var sLog = "Application";
+                var sEvent = "Sample Event";
+
+                if (!EventLog.SourceExists(sSource))
+                    EventLog.CreateEventSource(sSource, sLog);
+
+                EventLog.WriteEntry(sSource, sEvent);
+
+                EventLog.WriteEntry(sSource, sEvent, EventLogEntryType.Warning, 234);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Message: {ex.Message}");
+                Console.WriteLine($"Exception: {ex.InnerException?.ToString() ?? ""}");
+                Console.WriteLine(
+                    "If you receive this error, running this sample code with Administrator Rights may resolve the issue by allowing the 'Source' to be created.");
+            }
+            finally
+            {
+                Console.WriteLine("--Done--");
+                Console.ReadLine();
+            }
+        }
+
+
     }
 }
